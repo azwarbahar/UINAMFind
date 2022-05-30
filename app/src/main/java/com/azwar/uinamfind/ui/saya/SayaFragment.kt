f4220c7b1b379fcf20c40efce09258abaf17f32f
azwarbahar07 <azwarbahar07@gmail.com>
2022-05-30 23:39:08 +0800

    dhdhj

--- app/src/main/java/com/azwar/uinamfind/ui/saya/SayaFragment.kt
+++ app/src/main/java/com/azwar/uinamfind/ui/saya/SayaFragment.kt
@@ -6,9 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.azwar.uinamfind.R
+import com.azwar.uinamfind.databinding.FragmentSayaBinding
+import com.azwar.uinamfind.ui.home.HomeFragment
+import com.azwar.uinamfind.ui.mahasiswa.adapter.KeahlianAdapter
+import com.azwar.uinamfind.ui.mahasiswa.adapter.OrganisasiMahasiswaAdapter
+import com.azwar.uinamfind.ui.mahasiswa.adapter.PendidikanAdapter
+import com.azwar.uinamfind.ui.mahasiswa.adapter.PengalamanAdapter
 
 class SayaFragment : Fragment() {
 
+    private var _sayaBinding: FragmentSayaBinding? = null
+    private val sayaBinding get() = _sayaBinding!!
+
+    private lateinit var keahlianAdapter: KeahlianAdapter
+
+    private lateinit var pengalamanAdapter: PengalamanAdapter
+
+    private lateinit var pendidikanAdapter: PendidikanAdapter
+
+    private lateinit var organisasiMahasiswaAdapter: OrganisasiMahasiswaAdapter
+
+    companion object {
+        fun newInstance() = SayaFragment()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,11 +36,10 @@ class SayaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val view: View = inflater.inflate(
-            R.layout.fragment_saya, container,
-            false
-        )
+        _sayaBinding = FragmentSayaBinding.inflate(inflater, container, false)
 
-        return view
+
+
+        return sayaBinding.root
     }
 }
